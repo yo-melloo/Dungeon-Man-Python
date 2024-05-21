2f421720de75c8dd5707f8774a7c25384f593f78
Gustavo Mello <oliver7mello@gmail.com>
2024-05-21 16:17:34 -0300

    Adicionei a funcão de criar personagem
    a def "createCharacter()" ainda precisa de uns aperfeiçoamentos no momento...

--- main.py
+++ main.py
@@ -8,6 +8,49 @@ import spawner as sp
 import market
 import interations as ui
 
+# -- "funções" --
+
+# Criar Personagem: ---
+def createCharacter():
+    playerClassWarrior = {'class':'Guerreiro','hp':280,'atk':80,'def':45,'spd':20,'spc_attack':'NONE'}
+    playerClassShooter = {'class':'Atirador','hp':200,'atk':95,'def':30,'spd':35,'spc_attack':'NONE'}
+    playerClassMage = {'class':'Mago','hp':220,'atk':70,'def':40,'spd':35,'spc_attack':'Fire Ball'}
+    playerClassDemon = {'class':'Demônio','hp':310,'atk':90,'def':55,'spd':40,'spc_attack':'Diabolic Punch'}
+    
+    print('\n### Classes ###\n')
+    print(f'CLASSE: {playerClassWarrior["class"]}\nDESCRIÇÃO: \nATRIBUTOS:\nSAÚDE: {playerClassWarrior["hp"]}\nATAQUE: {playerClassWarrior["atk"]}\nDEFESA: {playerClassWarrior["def"]}\nVELOCIDADE: {playerClassWarrior["spd"]}\n')
+    print(f'CLASSE: {playerClassShooter["class"]}\nDESCRIÇÃO: \nATRIBUTOS:\nSAÚDE: {playerClassShooter["hp"]}\nATAQUE: {playerClassShooter["atk"]}\nDEFESA: {playerClassShooter["def"]}\nVELOCIDADE: {playerClassShooter["spd"]}\n')
+    print(f'CLASSE: {playerClassMage["class"]}\nDESCRIÇÃO: \nATRIBUTOS:\nSAÚDE: {playerClassMage["hp"]}\nATAQUE: {playerClassMage["atk"]}\nDEFESA: {playerClassMage["def"]}\nVELOCIDADE: {playerClassMage["spd"]}\n')
+    print(f'CLASSE: {playerClassDemon["class"]}\nDESCRIÇÃO: \nATRIBUTOS:\nSAÚDE: {playerClassDemon["hp"]}\nATAQUE: {playerClassDemon["atk"]}\nDEFESA: {playerClassDemon["def"]}\nVELOCIDADE: {playerClassDemon["spd"]}\n')
+    
+    playerInputChoose = input('> Digite o nome da classe escolhida: ')
+    
+    if (playerInputChoose.capitalize() == playerClassWarrior['class']):
+        return playerClassWarrior
+    if (playerInputChoose.capitalize() == playerClassShooter['class']):
+        return playerClassShooter
+    if (playerInputChoose.capitalize() == playerClassMage['class']):
+        return playerClassMage
+    if (playerInputChoose.capitalize() == playerClassDemon['class']):
+        return playerClassDemon
+# Criar Personagem fim ---
+
+# -- "funções" --
+
+
+#Saudações // Menu Principal
+print('### Bem-vindo a Dungeon Man ###')
+print('--- ver 1.5 - mai 2024 ---')
+start = input('Deseja iniciar o jogo? (S/N): ')
+if (start.upper() == "N" or start == "NÃO"):
+    exit()
+else:
+    playerName = input('> Digite um nome para seu personagem: ')
+    print(f'\n>> Parabens! você criou: {playerName}, mas é preciso definir qual classe ele(a) pertence.')
+    player = createCharacter()
+    print(f"Isso! {playerName} é da classe {player['class']}!")
+
+
 # Etapas e Funcionalidades do Jogo:
 # Etapa 1 - Criar personagem
     # Sistema de Classes
