5626512a5a0834aedfef822d06d3da491080fe7b
Gustavo Mello <oliver7mello@gmail.com>
2024-05-21 13:54:16 -0300

    Adicionado objetivos na main
    No documento MAIN.PY foi escrito comentários para orientar no desenvolvimento do projeto

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
+'''
+DUNGEON MAN 1.5 - 2024 ver
+Author: Gustavo Mello
+Description: RPG Experimental Backend Game
+'''
+
 import spawner as sp
 import market
+import interations as ui
+
+# Etapas e Funcionalidades do Jogo:
+# Etapa 1 - Criar personagem
+    # Sistema de Classes
+    # Sistema de Atributos
+# Etapa 2 - Lutar contra um inimigo
+    # Sistema de Luta
+    # Sistema de especiais
+    # Sistema de fuga
+# Etapa 3 - Subir de nível
+    # Funcionalidades interativas com Sis. de Atributos
+    # Multiplicador de dificuldade
+# Etapa 4 - Comprar items
+    # Sistema de loja
+    # Sistema de economia
+    # Sistema de troca
